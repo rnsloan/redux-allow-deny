f30469d6b5bb9f4b41acc457bf6be0c77b19810d
Chris Shepherd <chris@chrisshepherd.me>
2017-03-09 23:43:28 +0000

    Refactor to use internal function

--- src/index.js
+++ src/index.js
@@ -1,48 +1,44 @@
-export const blacklist = (blacklistActions, callback) => {
-  return store => next => action => {
-    const result = next(action)
+const callbackOnPredicate = (predicate, callback) =>
+  store => next => action => {
+    const result = next(action);
 
+    if (predicate(action.type)) {
+      callback(action, store);
+    }
+
+    return result;
+  };
+
+export const blacklist = (blacklistActions, callback) => {
+  if (process.env.NODE_ENV !== 'production') {
     if (!Array.isArray(blacklistActions)) {
-      console.error('blacklist middleware expected array as first argument:', blacklistActions)
-      return result
+      throw new Error('blacklist middleware expected array as first argument');
     }
 
     if (typeof callback !== 'function') {
-      console.error('blacklist middleware expected function as callback:', callback)
-      return result
+      throw new Error('blacklist middleware expected function as callback');
     }
-
-    if (blacklistActions.indexOf(action.type) === -1) {
-      callback(action, store)
-    }
-
-    return result
   }
-}
+
+  return callbackOnPredicate(
+    type => blacklistActions.indexOf(type) === -1,
+    callback
+  );
+};
 
 export const whitelist = (whitelistActions, callback) => {
-  return store => next => action => {
-    const result = next(action)
-
+  if (process.env.NODE_ENV !== 'production') {
     if (!Array.isArray(whitelistActions)) {
-      console.error('whitelist middleware expected array as first argument:', whitelistActions)
-      return result
+      throw new Error('whitelist middleware expected array as first argument');
     }
 
     if (typeof callback !== 'function') {
-      console.error('whitelist middleware expected function as callback:', callback)
-      return result
+      throw new Error('whitelist middleware expected function as callback');
     }
-
-    if (whitelistActions.indexOf(action.type) !== -1) {
-      callback(action, store)
-    }
-
-    return result
   }
-}
 
-export default {
-  blacklist: blacklist,
-  whitelist: whitelist,
-}
+  return callbackOnPredicate(
+    type => whitelistActions.indexOf(type) !== -1,
+    callback
+  );
+};
